0aaa21714413ab366be88d04600c67f20461419e
Volodymyr Romanenko <98472440+Mr-Nihility@users.noreply.github.com>
2022-09-03 11:47:05 +0300

    final

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
-const fs = require("fs/promises");
-console.log("hello, vov4ik");
+const { Command } = require("commander");
+const {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts.js");
 
-console.log("hello, Sasha");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
-console.log("hello, Rom4ik");
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: рефакторить
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      console.table(await listContacts());
+      break;
+
+    case "get":
+      console.table(await getContactById(id));
+      break;
+
+    case "add":
+      await addContact(name, email, phone);
+      console.table(await listContacts());
+      break;
+
+    case "remove":
+      await removeContact(id);
+      console.table(await listContacts());
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
